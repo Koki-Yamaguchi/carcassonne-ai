c643df2dfd5e3c061a58ddac4d75c7324b6de8e2
Koki Yamaguchi <yk49at21@gmail.com>
2023-10-17 09:18:23 +0900

    frontend: Show vote count on problem list

--- frontend/src/views/ProblemsView.vue
+++ frontend/src/views/ProblemsView.vue
@@ -5,12 +5,16 @@ import { API } from "../api";
 import { Problem, Player } from "../types";
 import { translate } from "../locales/translate";
 import { store } from "../store";
+import SpinnerIcon from "../components/SpinnerIcon.vue";
 
 const problems = ref<Problem[]>([]);
 const voted = ref<boolean[]>([]);
+const voteCounts = ref<number[]>([]);
 const player = ref<Player | null>(null);
+const loading = ref<boolean>(false);
 
 onMounted(async () => {
+  loading.value = true;
   const api = new API();
 
   player.value = await api.getPlayerByUserID(store.userID);
@@ -20,13 +24,12 @@ onMounted(async () => {
   const votedProblemIDs = votes
     .filter((v) => v.playerID == player.value?.id)
     .map((v) => v.problemID);
-  problems.value.forEach((problem) => {
-    if (votedProblemIDs.includes(problem.id)) {
-      voted.value.push(true);
-    } else {
-      voted.value.push(false);
-    }
-  });
+  voted.value = problems.value.map((p) => votedProblemIDs.includes(p.id));
+  voteCounts.value = problems.value.map(
+    (p) => votes.filter((v) => v.problemID === p.id).length
+  );
+
+  loading.value = false;
 });
 </script>
 
@@ -36,11 +39,13 @@ onMounted(async () => {
     <p class="my-2 text-sm text-gray-700">
       {{ translate("problems_description") }}
     </p>
-    <div v-if="voted.length > 0" class="mt-4">
+    <div v-if="loading"><SpinnerIcon /></div>
+    <div v-else class="mt-4">
       <ProblemItem
         v-for="(problem, idx) in problems"
         :problem="problem"
         :voted="voted[idx]"
+        :voteCount="voteCounts[idx]"
         :key="problem.id"
       />
     </div>
