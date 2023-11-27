ff27564f341d75c43c650ea8731b4aa66fcc6508
Koki Yamaguchi <yk49at21@gmail.com>
2023-11-27 17:47:00 +0900

    frontend: Add problem proposal link to problems page

--- frontend/src/locales/translate.ts
+++ frontend/src/locales/translate.ts
@@ -2,10 +2,14 @@ import * as ja from "../locales/ja.json";
 import * as en from "../locales/en.json";
 import { store } from "../store";
 
-export const translate = (s: keyof typeof ja) => {
+export const isJapaneseSetting = () => {
   const lang =
     store.language !== "" ? store.language : window.navigator.language;
-  if (lang === "ja") {
+  return lang === "ja";
+};
+
+export const translate = (s: keyof typeof ja) => {
+  if (isJapaneseSetting()) {
     return ja[s];
   } else {
     return en[s];
