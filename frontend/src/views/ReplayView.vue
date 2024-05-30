c99f7b79a5e430be7412fc2092a1f88ecfdced1a
Koki Yamaguchi <yk49at21@gmail.com>
2024-05-31 00:09:55 +0900

    frontend: Fix replay

--- frontend/src/views/ReplayView.vue
+++ frontend/src/views/ReplayView.vue
@@ -3,7 +3,17 @@ import { computed, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { API } from "../api";
 import { newTile, Tile, TileKind } from "../tiles";
-import { Board, DiscardMove, Game, TileMove, Player } from "../types";
+import {
+  Board,
+  DiscardMove,
+  Game,
+  TileMove,
+  Player,
+  Move,
+  MeepleMove,
+  TilePosition,
+  CompleteEvent,
+} from "../types";
 import { translate } from "../locales/translate";
 import GameBoard from "../components/GameBoard.vue";
 import PlayerInfo from "../components/PlayerInfo.vue";
@@ -27,6 +37,16 @@ const player1ProfileImageURL = ref<string>("");
 const tileCount = ref<number>(1);
 const discardedTileKinds = ref<TileKind[]>([]);
 const showDiscardedTiles = ref<boolean>(false);
+const moves = ref<Move[]>([]);
+const meepledPositions = ref<Map<number, TilePosition>>(new Map());
+const player0LastTilePos = ref<TilePosition>({ y: -1, x: -1 });
+const player1LastTilePos = ref<TilePosition>({ y: -1, x: -1 });
+const updating = ref<boolean>(false);
+const finished = ref<boolean>(false);
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const initGame = async () => {
   const api = new API();
@@ -43,8 +63,9 @@ const initGame = async () => {
   player0ProfileImageURL.value = player0.profileImageURL;
   player1ProfileImageURL.value = player1.profileImageURL;
 
-  const moves = await api.getMoves(game.value.id);
-  maxMoveOrd.value = moves[moves.length - 1].ord;
+  moves.value = await api.getMoves(game.value.id);
+
+  maxMoveOrd.value = moves.value[moves.value.length - 1].ord;
   currentMoveOrd.value = maxMoveOrd.value;
 };
 
@@ -53,9 +74,57 @@ const resetMeeples = () => {
   player1Meeples.value = new Set([7, 8, 9, 10, 11, 12, 13]);
 };
 
+const retrieveMeeple = (meeples: Set<number>, meeple: number): TilePosition => {
+  meeples.add(meeple);
+  const pos = meepledPositions.value.get(meeple);
+  if (!pos) {
+    return { y: -1, x: -1 };
+  }
+  return pos;
+};
+
+const processCompleteEvents = (completeEvents: CompleteEvent[]) => {
+  const pos = [];
+  for (const e of completeEvents) {
+    let player0Count = 0;
+    let player1Count = 0;
+    for (const meepleID of e.meepleIDs) {
+      if (meepleID < 7) {
+        player0Count++;
+        pos.push(retrieveMeeple(player0Meeples.value, meepleID));
+      } else {
+        player1Count++;
+        pos.push(retrieveMeeple(player1Meeples.value, meepleID));
+      }
+    }
+    if (player0Count >= player1Count) {
+      player0Point.value += e.point;
+    }
+    if (player1Count >= player0Count) {
+      player1Point.value += e.point;
+    }
+  }
+  for (const p of pos) {
+    tiles.value[p.y][p.x]?.removeMeeple();
+  }
+};
+
+const finishGame = async () => {
+  if (!game.value) {
+    return;
+  }
+  const api = new API();
+
+  const finalEvents = await api.getFinalEevnts(game.value.id);
+
+  processCompleteEvents(finalEvents.completeEvents);
+
+  finished.value = true;
+};
+
 const useMeeple = (
   meeples: Set<number>,
-  // pos: TilePosition,
+  pos: TilePosition,
   meepleID?: number
 ): number => {
   let mid = meepleID ? meepleID : -1;
@@ -66,12 +135,98 @@ const useMeeple = (
     }
   }
   meeples.delete(mid);
-  // meepledPositions.value.set(mid, pos);
+  meepledPositions.value.set(mid, pos);
 
   return mid;
 };
 
-const update = async () => {
+const next = async () => {
+  if (!game.value || !player.value) {
+    return;
+  }
+  updating.value = true;
+
+  const api = new API();
+
+  const tileMove = moves.value[currentMoveOrd.value + 1] as TileMove;
+
+  tiles.value[tileMove.pos.y][tileMove.pos.x] = newTile(
+    tileMove.rot,
+    tileMove.tile,
+    null,
+    -1,
+    -1,
+    player.value.tileEdition
+  );
+
+  await sleep(500);
+
+  const meepleMove = moves.value[currentMoveOrd.value + 2] as MeepleMove;
+
+  if (meepleMove.meepleID !== -1) {
+    const meepleColor =
+      tileMove.playerID === game.value.player0ID
+        ? game.value.player0Color
+        : game.value.player1Color;
+    tiles.value[tileMove.pos.y][tileMove.pos.x]?.placeMeeple(
+      meepleMove.pos,
+      meepleColor,
+      meepleMove.meepleID
+    );
+    useMeeple(
+      tileMove.playerID === game.value.player0ID
+        ? player0Meeples.value
+        : player1Meeples.value,
+      tileMove.pos,
+      meepleMove.meepleID
+    );
+  }
+
+  if (tileMove.playerID === game.value.player0ID) {
+    if (player0LastTilePos.value.y !== -1) {
+      tiles.value[player0LastTilePos.value.y][
+        player0LastTilePos.value.x
+      ]?.addFrame(null);
+    }
+    tiles.value[tileMove.pos.y][tileMove.pos.x]?.addFrame(
+      game.value.player0Color
+    );
+    player0LastTilePos.value = { y: tileMove.pos.y, x: tileMove.pos.x };
+  } else {
+    if (player1LastTilePos.value.y !== -1) {
+      tiles.value[player1LastTilePos.value.y][
+        player1LastTilePos.value.x
+      ]?.addFrame(null);
+    }
+    tiles.value[tileMove.pos.y][tileMove.pos.x]?.addFrame(
+      game.value.player1Color
+    );
+    player1LastTilePos.value = { y: tileMove.pos.y, x: tileMove.pos.x };
+  }
+
+  const board = await api.getBoard(
+    game.value.id,
+    game.value.player0Color,
+    game.value.player1Color,
+    player.value.tileEdition,
+    currentMoveOrd.value + 2
+  );
+
+  await sleep(500);
+
+  processCompleteEvents(board.completeEvents);
+
+  currentMoveOrd.value += 2;
+  tileCount.value++;
+
+  if (currentMoveOrd.value == maxMoveOrd.value) {
+    finishGame();
+  }
+
+  updating.value = false;
+};
+
+const refresh = async () => {
   if (!game.value || !player.value) {
     return;
   }
@@ -98,9 +253,9 @@ const update = async () => {
         const meepleID = tiles.value[y][x]?.meepleID;
         if (meepleID !== -1) {
           if ((meepleID as number) < 7) {
-            useMeeple(player0Meeples.value, meepleID);
+            useMeeple(player0Meeples.value, { y, x }, meepleID);
           } else {
-            useMeeple(player1Meeples.value, meepleID);
+            useMeeple(player1Meeples.value, { y, x }, meepleID);
           }
         }
       }
@@ -141,6 +296,8 @@ const update = async () => {
     player1Point.value = game.value.player1Point;
     resetMeeples();
   }
+
+  finished.value = currentMoveOrd.value === maxMoveOrd.value;
 };
 
 const winner = computed(() => {
@@ -155,9 +312,31 @@ const winner = computed(() => {
   }
 });
 
+const currentTileSrc = () => {
+  if (currentMoveOrd.value === maxMoveOrd.value || !player.value) {
+    return "";
+  }
+
+  const tileMove = moves.value[currentMoveOrd.value + 1] as TileMove;
+  return newTile(0, tileMove.tile, null, -1, -1, player.value.tileEdition).src;
+};
+
+const currentPlayerName = () => {
+  if (currentMoveOrd.value === maxMoveOrd.value || !game.value) {
+    return "";
+  }
+
+  const tileMove = moves.value[currentMoveOrd.value + 1] as TileMove;
+
+  if (tileMove.playerID === game.value.player0ID) {
+    return game.value.player0Name;
+  }
+  return game.value.player1Name;
+};
+
 onMounted(async () => {
   await initGame();
-  await update();
+  await refresh();
 });
 </script>
 
@@ -168,8 +347,11 @@ onMounted(async () => {
     <div
       @click="
         () => {
+          if (updating) {
+            return;
+          }
           currentMoveOrd = 1;
-          update();
+          refresh();
         }
       "
     >
@@ -178,8 +360,14 @@ onMounted(async () => {
     <div
       @click="
         () => {
-          currentMoveOrd = Math.max(currentMoveOrd - 1, 1);
-          update();
+          if (updating) {
+            return;
+          }
+          if (currentMoveOrd === 1) {
+            return;
+          }
+          currentMoveOrd -= 2;
+          refresh();
         }
       "
     >
@@ -188,8 +376,13 @@ onMounted(async () => {
     <div
       @click="
         () => {
-          currentMoveOrd = Math.min(currentMoveOrd + 1, maxMoveOrd);
-          update();
+          if (updating) {
+            return;
+          }
+          if (currentMoveOrd === maxMoveOrd) {
+            return;
+          }
+          next();
         }
       "
     >
@@ -199,8 +392,11 @@ onMounted(async () => {
     <div
       @click="
         () => {
+          if (updating) {
+            return;
+          }
           currentMoveOrd = maxMoveOrd;
-          update();
+          refresh();
         }
       "
     >
@@ -242,7 +438,20 @@ onMounted(async () => {
       </div>
     </div>
   </div>
-  <div v-if="currentMoveOrd === maxMoveOrd">
+  <div v-if="!finished">
+    <div
+      class="bg-gray-100 rounded text-gray-900 text-sm px-4 py-3 shadow-md flex items-center gap-2"
+    >
+      <span>{{ currentPlayerName() }}{{ translate("must_place") }}</span>
+      <img
+        class="min-h-[30px]"
+        width="30"
+        height="30"
+        :src="currentTileSrc()"
+      />
+    </div>
+  </div>
+  <div v-if="finished">
     <div class="bg-gray-100 rounded text-gray-900 px-4 py-3 shadow-md">
       <p class="flex flex-col justify-center mr-3">
         {{ winner }} {{ translate("wins") }}
