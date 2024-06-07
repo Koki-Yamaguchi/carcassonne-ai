1a1e7a21323af080580dfb229c1e7304159ca4b2
Koki Yamaguchi <yk49at21@gmail.com>
2024-06-07 17:19:32 +0900

    frontend: Fix profile image style on game result page

--- frontend/src/components/GameResultInfo.vue
+++ frontend/src/components/GameResultInfo.vue
@@ -13,31 +13,37 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex gap-4 w-full border rounded-xl hover:bg-gray-50 p-4">
-    <div class="flex flex-col justify-center">
-      <ProfileImage :src="profileImage" />
-    </div>
-    <div class="w-full">
-      <div class="flex justify-between">
-        <div class="flex">
-          <img v-if="isWinner" class="w-5 mr-1" src="../assets/img/crown.png" />
-          <div>{{ name }}</div>
+  <div
+    class="flex gap-4 w-full border rounded-xl hover:bg-gray-50 p-4 items-center"
+  >
+    <ProfileImage :src="profileImage" />
+    <div class="w-[calc(100%-48px)] flex justify-between items-center">
+      <div>
+        <div class="flex justify-between">
+          <div class="flex">
+            <img
+              v-if="isWinner"
+              class="w-5 mr-1"
+              src="../assets/img/crown.png"
+            />
+            <div>{{ name }}</div>
+          </div>
         </div>
-        <div>{{ point }} pt</div>
+        <div class="flex mt-2">
+          <div>{{ beforeRating ?? 1500 }}</div>
+          <ArrowIcon
+            :color="
+              !beforeRating || !afterRating || beforeRating == afterRating
+                ? 'gray'
+                : afterRating > beforeRating
+                ? 'green'
+                : 'red'
+            "
+          />
+          <div>{{ afterRating ?? 1500 }}</div>
+        </div>
       </div>
-      <div class="flex mt-2">
-        <div>{{ beforeRating ?? 1500 }}</div>
-        <ArrowIcon
-          :color="
-            !beforeRating || !afterRating || beforeRating == afterRating
-              ? 'gray'
-              : afterRating > beforeRating
-              ? 'green'
-              : 'red'
-          "
-        />
-        <div>{{ afterRating ?? 1500 }}</div>
-      </div>
+      <div>{{ point }} pt</div>
     </div>
   </div>
 </template>
